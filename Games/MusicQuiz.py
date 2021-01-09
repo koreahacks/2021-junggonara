7e76f94e8badf88a42cecf164840c04c796088e7
gurwns9325@naver.com <toa94821046!>
2021-01-10 07:00:04 +0900

    노래 문제까지 하는거 성공 노래 갯수 늘리기 readme.md가 작성되지 않아서 readme.txt로 작성

--- Games/MusicQuiz.py
+++ Games/MusicQuiz.py
@@ -1,7 +1,6 @@
 import discord
-import ffmpeg
 import asyncio
-from discord.ext import commands
+import time
 import re
 import json
 from random import randrange
@@ -19,34 +18,41 @@ async def musicPlay(message, bot):
                         url)  # 정규 표현식을 사용해 url 검사
 
     voice_client = await VoiceController.connect_bot_voice_channel(message)
-
+    start_time = time.time()
     voice_client.play(discord.FFmpegPCMAudio(executable="C:/ffmpeg-4.3.1-2021-01-01-full_build-shared/bin/ffmpeg.exe", source="./music/IU_Good.mp3"))
-
+    while 1:
+        if time.time()-start_time > 3.0 :
+            break
     voice_client.stop()
     return [url,singer,title]
 
 async def musicQ(message, LIST, bot):
     channel = message.channel
     embedSelect = discord.Embed(title="문제 목록")
-    embedSelect.add_field(name="1. 음악이름      2. 가수", value="원하는 문제목록을 골라주세요\nex) 1.음악")
+    embedSelect.add_field(name="1. 음악이름      2. 가수", value="원하는 문제목록을 골라주세요\nex) 1 or 2")
     await channel.send(embed=embedSelect)
-
+    channel = message.channel
     def pred(m):
         return m.author == message.author and m.channel == message.channel
     msg = await bot.wait_for('message', check=pred)
+
 
     print("abc2")
     print(msg)
     Set= await musicPlay(message, bot)
     answer="answer"
-    #if msg.startswitch('2'):
-    #    answer=Set[1]
-    #    await channel.send('2')
-    #else:
-    #    answer=Set[2]
-    #    await channel.send('1')
-    #while 1:
-    #    msg = await bot.wait_for('message', check=pred)
-    #    if msg == answer:
-    #        print(msg.author)
-    #        break
+    if msg.content=="2":
+        answer=Set[1]
+        await channel.send('2')
+    else:
+        answer=Set[2]
+        await channel.send('1')
+    while 1:
+        msg = await bot.wait_for('message', check=pred)
+        if msg.content == answer:
+            embed=discord.Embed(title="정답자")
+            embed.add_field(name="정답은 "+Set[1]+" 의 "+Set[2]+"입니다.",value=f"<@{msg.author}>이(가) 맞췄습니다.",inline=False)
+            embed.set_footer()
+            await channel.send(embed=embed)
+            print(msg.author)
+            break
---
+++ Readme.txt
@@ -0,0 +1,3 @@
+pip install -U discord.py[voice]
+pip install ffmpeg
+https://www.gyan.dev/ffmpeg/builds/ ->ffmpeg 설치
