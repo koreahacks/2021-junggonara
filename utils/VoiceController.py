4cf361725c20b7d2c6dd7fb0e3c80bc4d7d593db
girinssh <tngus5851@naver.com>
2021-01-09 20:21:40 +0900

    Create Voice Controller
    voice controller can switch speaker and mic

--- utils/VoiceController.py
+++ utils/VoiceController.py
@@ -1,21 +1,10 @@
 import discord
 from discord.ext import commands
 
-class VoiceController:
-    def __init__(self, bot:commands.Bot):
-        self.bot = bot
 
-    def setSpeaker(self, voicechannel:discord.VoiceChannel, user_id:int, state):
-        user = ''
-        for u in voicechannel.members:
-            if discord.Member(u).id == user_id:
-                u.voice.deaf = state
-                return
+async def set_speaker(member: discord.Member, state: bool):
+    await member.edit(deafen=not state)
 
-    def setMic(self, voicechannel:discord.VoiceChannel, user_id:int, state):
-        user = ''
-        for u in voicechannel.members:
-            if discord.Member(u).id == user_id:
-                u.voice.mute = state
-                return
 
+async def set_mic(member: discord.Member, state: bool):
+    await member.edit(mute=not state)
