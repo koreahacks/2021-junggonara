026a4601e601627075a8073a262bb1dbaef2e16e
girinssh <tngus5851@naver.com>
2021-01-09 23:08:22 +0900

    Create GameManager Class
    Edit on_message method at main.py

---
+++ utils/GameManager.py
@@ -0,0 +1,51 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+
+class GameManager:
+    __instance = None
+
+    @classmethod
+    def __getInstance(cls):
+        return cls.__instance
+
+    @classmethod
+    def instance(cls, *args, **kargs):
+        cls.__instance = cls(*args, **kargs)
+        cls.instance = cls.__getInstance
+        return cls.__instance
+
+    def __init__(self):
+        self.game_state = "WAIT_GAME"
+        self.game_name = ""
+        self.users = []
+        self.next_user = None
+
+    async def recruit(self, message: discord.Message, bot: commands.Bot,  count: float, game_title: str, min=0):
+        self.users = []
+        self.users.append(message.author)
+
+        channel = message.channel
+
+        embed = discord.Embed(title="참가자 모집 시작",
+                              description=f"이모지를 눌러주세요!\n" + str(count) + "초 후에 " + game_title + "가 시작합니다!")
+        embed.set_footer(text="명령어를 호출한 사용자는 이미 등록되었습니다")
+        emoji = await channel.send(embed=embed)
+        await emoji.add_reaction('🔌')
+
+        try:
+            await bot.wait_for('대기시간', timeout=count)
+        except asyncio.TimeoutError:
+            if len(self.users) <= min:
+                await emoji.delete()
+                await channel.send(str(min) + "명 이하는 게임을 시작할 수 없어요!")
+                self.game_state = "GAME_OVER"
+            else:
+                await channel.send("게임을 시작합니다")
+
+    async def set_game_over(self, message: discord.Message, member: discord.Member):
+        self.game_state = "GAME_OVER"
+        self.next_user = member
+        await message.channel.send("!게임종료!")
+
